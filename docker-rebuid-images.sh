e3783803919dae79d8b1d2c66e383b1a5e8920c9
Sergey Notebook <kasser2005@gmail.com>
2019-11-18 23:32:28 +0300

    in progress

--- docker-rebuid-images.sh
+++ docker-rebuid-images.sh
@@ -1,2 +1,3 @@
+docker build -t k8app/backendapp --no-cache -f docker/backend/Dockerfile .
 docker build -t k8app/frontendapp --no-cache -f docker/frontend/Dockerfile .
 docker rmi $(docker images -f dangling=true -q)
